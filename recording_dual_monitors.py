b793b5679d08e02ed976fe6a2b02594fa5ff23fa
Andre-East <andre.eastwood.c@gmail.com>
2023-04-06 20:55:05 -0400

    Updated code

--- recording_dual_monitors.py
+++ recording_dual_monitors.py
@@ -7,11 +7,30 @@ from datetime import datetime
 import time
 from functools import partial
 import keyboard
-from plyer.utils import platform
-from plyer import notification
+from winotify import Notification, audio
 from pathlib import Path
 
 
+def notify(file_path, name):
+    # This if is used to check if the file actually exists after the video had been completed and then 
+    # displays a message via windows toast notifications, and if not it promps that file was not saved. 
+    if not Path(file_path).is_file():
+        toast = Notification(
+            app_id= 'PY Screen Recorder',
+            title='Failed Saved',
+            msg= name+' has not been saved',
+            duration= 'short',
+            icon=r'C:\Users\dreme\OneDrive\Documents\Mind Workout\Programming\local_code\Python\file_cleanup\Icon\python_94570.ico')       
+    else:
+        toast = Notification(
+            app_id= 'PY Screen Recorder',
+            title='Sucessfully Saved',
+            msg= name+' has been saved',
+            duration= 'short',
+            icon=r'C:\Users\dreme\OneDrive\Documents\Mind Workout\Programming\local_code\Python\file_cleanup\Icon\python_94570.ico')
+
+    toast.set_audio(audio.Default, loop=False)
+    toast.show()
 
 # These lines are used to get the resolution of the monitors from windows  
 user32 = ctypes.windll.user32
@@ -29,14 +48,14 @@ time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
 name = 'screen_record'+time_stamp
 
 # This line is used to create the file in the directory of choice 
-file_name = f'{name}.mp4'
+file_name = f'D:\Video of Projects and ScreenShots\{name}.mp4'
 
 
 # This line is used to render the video file format 
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
 # This line is used to write/create the video 
-captured_video = cv2.VideoWriter(file_name, fourcc, 7.9, (width, height))
+captured_video = cv2.VideoWriter(file_name, fourcc, 8.7, (width, height))
 
 # Vairables that will be intialized to help manage the speed of the recording
 fps = 60
@@ -44,9 +63,10 @@ prev = 0
 
 # This while loop will continuously take screenshots/record video until we end the recording 
 while True:
+    
     # This line is used store the time the loop began  
     time_elapsed = time.time() - prev
- 
+
     if time_elapsed > 1.0/fps:
 
         prev = time.time()
@@ -65,26 +85,16 @@ while True:
         # in order to create a continuous video 
         captured_video.write(img_final)
 
-        # This bool is then used to end the recording/stop screenshots 
-        if cv2.waitKey(1) and keyboard.is_pressed('q'):
-            break
+    # This bool is then used to end the recording/stop screenshots 
+    if cv2.waitKey(1) and keyboard.is_pressed('q'):
+        notify(file_name,name)
+        break
+
 # We then go ahead and destroy all windows      
 cv2.destroyAllWindows()
 
 # We also relase the view that was created from the script/app   
 captured_video.release()
 
-# This if is used to check if the file actually exists after the video had been completed and then 
-# displays a message via windows toast notifications, and if not it promps that file was not saved. 
-if not Path(file_name).is_file():
-    notification.notify(
-        title='Failed Saved',
-        message= name+' has not been saved',
-        app_name='PY Screen Recorder',
-        app_icon=r'\path\to\python_94570.ico')    
-else:
-    notification.notify(
-        title='Sucessfully Saved',
-        message= name+' has been saved',
-        app_name='PY Screen Recorder',
-        app_icon=r'\path\to\python_94570.ico')
+
+       
